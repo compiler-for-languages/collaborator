6fbaa69806f600f3d029e576d4a482c07f925f9b
SIDRAMAPPA773 <sidramhalannavar@gmail.com>
2025-04-20 22:44:07 +0530

    nuehh

--- newtest.c
+++ newtest.c
@@ -4,4 +4,6 @@ main()
     printf("Iam SKG, and this is my statement\n");
     printf("Iam siddram");
     printf("Iam Siddram 2nd time");
+    printf("hello how are you");
+    scanf("%s",iamfine);
 }
